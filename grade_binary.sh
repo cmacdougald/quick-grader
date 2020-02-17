955c8a51637981f05a4527a79dbad02a226dbcd7
Chris MacDougald <cmacdougald@alamo.edu>
2020-02-17 03:22:23 -0600

    Added binary grader.

--- grade_binary.sh
+++ grade_binary.sh
@@ -1,3 +1,18 @@
 #/bin/bash
 
+ordbin() {
+  a=$(printf '%d' "'$1")
+  echo "obase=2; $a" | bc
+}
+
+ascii2bin() {
+    echo -n $* | while IFS= read -r -n1 char
+    do
+        ordbin $char | tr -d '\n'
+        echo " "
+    done
+}
+
 read -p "Enter string to convert: " STRING_INPUT
+
+ascii2bin $STRING_INPUT
